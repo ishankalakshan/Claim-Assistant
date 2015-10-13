1eb92af2f951684453d8b8b6f826a88d8926d148
Ishanka Lakshan Wasalage <isankalakshan@gmail.com>
2015-10-13 16:31:15 +0530

    commit initial

--- 8.1App/ClaimAssistantApp/ClaimAssistantApp/Views/Garages.xaml.cs
+++ 8.1App/ClaimAssistantApp/ClaimAssistantApp/Views/Garages.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using ClaimAssistantApp.Common;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using ModelLayer;
 using Newtonsoft.Json.Linq;
-using System.Collections.ObjectModel;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
